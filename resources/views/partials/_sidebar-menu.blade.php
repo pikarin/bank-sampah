bb2a85d40432f2888030039b4c71cf8e6bdd58cd
Aditia <aditia@konsep.net>
2018-12-01 14:08:28 +0700

    Seperate sidebar menu into its own partial blade view

---
+++ resources/views/partials/_sidebar-menu.blade.php
@@ -0,0 +1,50 @@
+<nav class="mt-2">
+    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+      {{-- Add icons to the links using the .nav-icon class
+         with font-awesome or any other icon font library --}}
+         <li class="nav-item">
+            <a href="/home" class="nav-link active">
+                <i class="nav-icon fas fa-tachometer-alt"></i>
+                <p>Dashboard</p>
+            </a>
+        </li>
+
+        <li class="nav-item has-treeview">
+            <a href="#" class="nav-link">
+                <i class="nav-icon fas fa-cog"></i>
+                <p>
+                    Management
+                    <i class="right fa fa-angle-left"></i>
+                </p>
+            </a>
+            <ul class="nav nav-treeview">
+                <li £class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="fas fa-circle nav-icon"></i>
+                        <p>Data Sampah</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="fas fa-circle nav-icon"></i>
+                        <p>Data Jenis Sampah</p>
+                    </a>
+                </li>
+            </ul>
+        </li>
+
+        <li class="nav-item">
+            <a href="#" class="nav-link">
+                <i class="nav-icon fas fa-user"></i>
+                <p>Profile</p>
+            </a>
+        </li>
+
+        <li class="nav-item">
+            <a href="#" class="nav-link">
+                <i class="nav-icon fas fa-power-off"></i>
+                <p>Logout</p>
+            </a>
+        </li>
+    </ul>
+</nav>
